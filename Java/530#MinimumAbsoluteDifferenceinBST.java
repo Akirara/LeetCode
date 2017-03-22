ae89797fe7183600253e07786cf1878212def5c5
Akirara <zhaoxvan@gmail.com>
2017-03-22 18:14:55 +0800

    March 22 2017

--- Java/530#MinimumAbsoluteDifferenceinBST.java
+++ Java/530#MinimumAbsoluteDifferenceinBST.java
@@ -1,3 +1,4 @@
+// inorder tranversal
 public class Solution {
     int minDiff = Integer.MAX_VALUE;
     TreeNode pre = null;
@@ -21,5 +22,33 @@ public class Solution {
         
         pre = root;
         inorder(root.right);
+    }
+}
+
+
+// pass left bound and right bound
+public class Solution {
+    int minDiff = Integer.MAX_VALUE;
+    
+    public int getMinimumDifference(TreeNode root) {
+        helper(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        
+        return minDiff;
+    }
+    
+    public void helper(TreeNode cur, int lb, int rb) {
+        if (cur == null) {
+            return;
+        }
+        
+        if (lb != Integer.MIN_VALUE) {
+            minDiff = Math.min(minDiff, cur.val - lb);
+        }
+        if (rb != Integer.MAX_VALUE) {
+            minDiff = Math.min(minDiff, rb - cur.val);
+        }
+        
+        helper(cur.left, lb, cur.val);
+        helper(cur.right, cur.val, rb);
     }
 }
